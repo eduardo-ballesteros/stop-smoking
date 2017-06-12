fff2bc98d9d8f9eeb8ca6b8cb703d968b08632b1
Eduardo Ballesteros <eduardo@Eduardos-iMac.local>
2017-06-11 17:56:18 -0700

    Update to run on port 3000

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var app = express();
 
-app.listen(80, function() {
-    console.log('Stop Smoking Bot-Server listening on port 80...');
+app.listen(3000, function() {
+    console.log('Stop Smoking Bot-Server listening on port 3000...');
 });
 
 app.get('/*', function(req, res) {
     var jsonResponse = [];
-    jsonResponse.push({ "text": "Hi. " + (Math.random() * 5 + 1).toFixed(0) + " is a lucky number..." });
+    jsonResponse.push({ "text": "Hi. " + (Math.random() * 100 + 1).toFixed(0) + " is a lucky number..." });
     res.send(jsonResponse);
 });
